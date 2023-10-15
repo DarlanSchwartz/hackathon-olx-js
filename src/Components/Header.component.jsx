a0c8fde387ccd4558472e4a99a28790965cc269e
Schwartz <darlanswtz@gmail.com>
2023-10-15 01:54:03 -0300

    Firules: header placeholder text animation

--- src/Components/Header.component.jsx
+++ src/Components/Header.component.jsx
@@ -1,57 +1,96 @@
 import styled from 'styled-components';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { PiSuitcaseSimpleDuotone } from 'react-icons/pi';
 import { BsGrid, BsChat, BsBell, BsSearch } from 'react-icons/bs';
 import OrangeButton from './OrangeButton';
 import { useNavigate } from 'react-router-dom';
 
+const PLACEHOLDER_SEARCH_WORDS = [
+  "Celular",
+  "Carro",
+  "Apartamento"
+];
+
 export default function Header({ show_sb, show_items, show_announce_btn }) {
-    const [search, setSearch] = useState('');
-    const navigate = useNavigate();
-    return (
-        <HeaderContainer>
-            <img src="/olx-logo.png" alt="" onClick={()=> navigate('/')} />
-            {
-                show_sb &&
-                <SearchBarContainer>
-                    <input type="text" placeholder="Buscar..." value={search} onChange={e => setSearch(e.target.value)} />
-                    <BsSearch className="icon" />
-                </SearchBarContainer>
-            }
-            {
-                show_items &&
-                <>
-                    <HeaderItem>
-                        <PiSuitcaseSimpleDuotone />
-                        <span>Plano Profisional</span>
-                    </HeaderItem>
-                    <HeaderItem onClick={()=> navigate('/my-announces')}>
-                        <BsGrid />
-                        <span>Meus anúncios</span>
-                    </HeaderItem>
-                    <HeaderItem>
-                        <BsChat />
-                        <span>Chat</span>
-                    </HeaderItem>
-                    <HeaderItem>
-                        <BsBell />
-                        <span>Notificações</span>
-                    </HeaderItem>
-                </>
-            }
-            <HeaderUser>
-                <div></div>
-                <span>Fulano</span>
-            </HeaderUser>
-            {
-                show_announce_btn &&
-                <OrangeButton onClick={() => navigate('/register')} style={{ maxWidth: "107.08px" }}>
-                    Anunciar
-                </OrangeButton>
-            }
-        </HeaderContainer>
-    )
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+  const [textoOriginal, setTextoOriginal] = useState(PLACEHOLDER_SEARCH_WORDS[0]);
+  const [textoAtual, setTextoAtual] = useState('');
+  const [index, setIndex] = useState(0);
+  const [wordIndex, setWordIndex] = useState(0);
+
+  useEffect(() => {
+    function adicionarLetra() {
+      if (index < textoOriginal.length) {
+        setTextoAtual((prevTexto) => prevTexto + textoOriginal[index]);
+        setIndex((prevIndex) => prevIndex + 1);
+      } else {
+        // Apagar o texto atual letra por letra
+        if (textoAtual.length > 0) {
+          setTextoAtual((prevTexto) => prevTexto.slice(0, -1));
+        } else {
+          // Quando o texto atual estiver vazio, mude para a próxima palavra
+          setIndex(0);
+          setWordIndex((prevWordIndex) => (prevWordIndex + 1) % PLACEHOLDER_SEARCH_WORDS.length);
+          setTextoOriginal(PLACEHOLDER_SEARCH_WORDS[(wordIndex + 1) % PLACEHOLDER_SEARCH_WORDS.length]);
+        }
+      }
+    }
+
+    const interval = setInterval(adicionarLetra, 200);
+
+    // Limpa o intervalo quando o componente for desmontado
+    return () => clearInterval(interval);
+  }, [index, textoOriginal, textoAtual, wordIndex]);
+
+
+  return (
+    <HeaderContainer>
+      <img src="/olx-logo.png" alt="" onClick={() => navigate('/')} />
+      {show_sb && (
+        <SearchBarContainer>
+          <input
+            type="text"
+            placeholder={("Buscar &quot;" + textoAtual + "&quot;").replace(/&quot;/g, '"')}
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <BsSearch className="icon" />
+        </SearchBarContainer>
+      )}
+      {show_items && (
+        <>
+          <HeaderItem>
+            <PiSuitcaseSimpleDuotone />
+            <span>Plano Profissional</span>
+          </HeaderItem>
+          <HeaderItem onClick={() => navigate('/my-announces')}>
+            <BsGrid />
+            <span>Meus anúncios</span>
+          </HeaderItem>
+          <HeaderItem>
+            <BsChat />
+            <span>Chat</span>
+          </HeaderItem>
+          <HeaderItem>
+            <BsBell />
+            <span>Notificações</span>
+          </HeaderItem>
+        </>
+      )}
+      <HeaderUser>
+        <div></div>
+        <span>Fulano</span>
+      </HeaderUser>
+      {show_announce_btn && (
+        <OrangeButton onClick={() => navigate('/register')} style={{ maxWidth: "107.08px" }}>
+          Anunciar
+        </OrangeButton>
+      )}
+    </HeaderContainer>
+  );
 }
+
 
 const HeaderUser = styled.div`
 width: 100%;
